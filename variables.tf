1dac1f4ab772861c022a774c1e514ed5aef95247
stephanerodrigo <79470701+stephanerodrigo@users.noreply.github.com>
2021-02-23 13:53:25 +0100

    update variables

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "db_table_name" {
   type    = string
   default = "terraform-learn"
 }
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
